beea349f20d2ab44c7e4d74c240473acb0e1fab5
092vk <vk092kumar@gmail.com>
2025-06-24 00:10:36 +0530

    added routes

---
+++ routes/routes.js
@@ -0,0 +1,6 @@
+// To DO: 
+
+// for future --- to add authentication, authorizationa and other complex logic related to routing
+// MVC structure will be followed
+// Redis for caching
+// Bulk insert in db using kafka for scalbility
